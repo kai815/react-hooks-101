4028b25e987e7600926b96a1a2ee219d449d9183
kai815 <h.kaizuka815@gmail.com>
2020-06-05 10:43:49 +0900

    微調整

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,6 @@ import AppContext from '../contexts/AppContext'
 
 import reducer from '../reducers'
 
-console.log(AppContext)
-
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
--- src/components/Events.js
+++ src/components/Events.js
@@ -1,4 +1,4 @@
-import React, { userContext, useContext } from 'react'
+import React, { useContext } from 'react'
 
 import Event from './Event'
 
