3c3e309fc9e6bad7fc7c2c8da5f8324ff685cf82
kai815 <h.kaizuka815@gmail.com>
2020-06-09 11:37:50 +0900

    不要なスペースや記述の削除

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -34,15 +34,15 @@ const EventForm = () => {
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
-          <label htmlFor="formEventTitle">タイト  ル</label>
-          <input className="form-control"   id="formEventTitle" value={title}   onChange={e => setTitle(e.target.value)}/>
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control"   id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
         <div className="form-group">
-          <label htmlFor="formEventBody">ボディー  </label>
-          <textarea className="form-control"   id="formEventBody" value={body}   onChange={e => setBody(e.target.value)  }/>
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control"   id="formEventBody" value={body} onChange={e => setBody(e.target.value) }/>
         </div>
         <button className="btn btn-primary"   onClick={addEvent} disabled=  {unCreatable} >イベントを作成する</button>
-        <button className="btn btn-danger"   onClick={deleteAllEvents} disabled=  {state.events.length === 0} >全てのイベントを削除  する</button>
+        <button className="btn btn-danger"   onClick={deleteAllEvents} disabled={state.events.length === 0} >全てのイベントを削除する</button>
       </form>
     </>
   )
